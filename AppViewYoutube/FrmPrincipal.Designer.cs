84335239e9e60ae15f3eab3f7142cb3a505c2788
Marcelo Marques <marcelomarqueslima.dev@outlook.com>
2023-01-18 19:01:07 -0300

    Add project files.

---
+++ AppViewYoutube/FrmPrincipal.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace AppViewYoutube
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.chkAutoPlay = new System.Windows.Forms.CheckBox();
+            this.btnCarregar = new System.Windows.Forms.Button();
+            this.lvlVideo = new System.Windows.Forms.ListView();
+            this.wbVideo = new System.Windows.Forms.WebBrowser();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lvlVideo, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.wbVideo, 0, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 70F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(948, 528);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 3;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 70F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel2.Controls.Add(this.txtUrl, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.chkAutoPlay, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.btnCarregar, 2, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(942, 46);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtUrl.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUrl.Location = new System.Drawing.Point(3, 3);
+            this.txtUrl.Multiline = true;
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(653, 40);
+            this.txtUrl.TabIndex = 0;
+            // 
+            // chkAutoPlay
+            // 
+            this.chkAutoPlay.AutoSize = true;
+            this.chkAutoPlay.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chkAutoPlay.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkAutoPlay.Location = new System.Drawing.Point(662, 3);
+            this.chkAutoPlay.Name = "chkAutoPlay";
+            this.chkAutoPlay.Size = new System.Drawing.Size(182, 40);
+            this.chkAutoPlay.TabIndex = 1;
+            this.chkAutoPlay.Text = "Autoplay";
+            this.chkAutoPlay.UseVisualStyleBackColor = true;
+            // 
+            // btnCarregar
+            // 
+            this.btnCarregar.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnCarregar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCarregar.Location = new System.Drawing.Point(850, 3);
+            this.btnCarregar.Name = "btnCarregar";
+            this.btnCarregar.Size = new System.Drawing.Size(89, 40);
+            this.btnCarregar.TabIndex = 2;
+            this.btnCarregar.Text = "Carregar";
+            this.btnCarregar.UseVisualStyleBackColor = true;
+            // 
+            // lvlVideo
+            // 
+            this.lvlVideo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lvlVideo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lvlVideo.HideSelection = false;
+            this.lvlVideo.Location = new System.Drawing.Point(3, 55);
+            this.lvlVideo.Name = "lvlVideo";
+            this.lvlVideo.Size = new System.Drawing.Size(942, 99);
+            this.lvlVideo.TabIndex = 1;
+            this.lvlVideo.UseCompatibleStateImageBehavior = false;
+            // 
+            // wbVideo
+            // 
+            this.wbVideo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.wbVideo.Location = new System.Drawing.Point(3, 160);
+            this.wbVideo.MinimumSize = new System.Drawing.Size(20, 20);
+            this.wbVideo.Name = "wbVideo";
+            this.wbVideo.Size = new System.Drawing.Size(942, 365);
+            this.wbVideo.TabIndex = 2;
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(948, 528);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "FrmPrincipal";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.CheckBox chkAutoPlay;
+        private System.Windows.Forms.Button btnCarregar;
+        private System.Windows.Forms.ListView lvlVideo;
+        private System.Windows.Forms.WebBrowser wbVideo;
+    }
+}
+
